cf0bf96a0d57d2a03b1bcd5fd79428cc4f0486e0
hatpoppo <toshi.hat@gmail.com>
2020-02-07 14:12:02 +0900

    ファイルを扱う部分のUIを実装

--- src/component/PhotoHeader.tsx
+++ src/component/PhotoHeader.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FilterTypes } from "../store";
-import { Stack, Text, Pivot, PivotItem, TextField, PrimaryButton } from "office-ui-fabric-react";
+import { Stack, Text, Pivot, PivotItem, TextField, PrimaryButton, Label, ILabel, IButton } from "office-ui-fabric-react";
 
 import { actions, actionsWithService } from "../actions";
 import { connect } from "react-redux";
@@ -10,11 +10,12 @@ interface PhotoHeaderProps {
   setFilter: (filter: FilterTypes) => void;
 }
 class PhotoHeader extends React.Component<PhotoHeaderProps, any> {
-  private myFile;
+  private _myFile;
+  private _defaultLabelFile = "ファイル名";
   constructor(props) {
     super(props);
-    this.state = { labelInput: "" };
-    this.myFile = React.createRef();
+    this.state = { labelInput: "", labelFile: this._defaultLabelFile };
+    this._myFile = React.createRef();
   }
   render() {
     return (
@@ -26,21 +27,42 @@ class PhotoHeader extends React.Component<PhotoHeaderProps, any> {
           <Stack.Item grow>
             <TextField value={this.state.labelInput} onChange={this._onChange} placeholder="タイトル"></TextField>
           </Stack.Item>
-          <PrimaryButton onClick={this._onClick}>追加</PrimaryButton>
         </Stack>
-        <input type="file" ref={this.myFile} />
+        <Stack horizontal>
+          <Stack.Item grow>
+            <TextField value={this.state.labelFile} disabled></TextField>
+          </Stack.Item>
+          <PrimaryButton onClick={this._onPhotoClick}>写真選択</PrimaryButton>
+        </Stack>
+        <PrimaryButton onClick={this._onClick} text="追加"></PrimaryButton>
+        <input type="file" ref={this._myFile} onChange={this._onPhotoChange} />
       </Stack>
     );
   }
   _onChange = evt => {
     this.setState({ labelInput: evt.target.value });
   };
-  _onClick = evt => {
-    const el = this.myFile.current!;
-    console.log(el);
+  _onPhotoClick = evt => {
+    let el = this._myFile.current;
     el.click();
-    let formData = new FormData();
-    formData.append("photo", el.files[0]);
+  };
+  _onPhotoChange = evt => {
+    const el = this._myFile.current;
+    if (el.files.length > 0) {
+      this.setState({ labelFile: el.files[0].name });
+    } else {
+      this.setState({ labelFile: this._defaultLabelFile });
+    }
+  };
+  _onClick = evt => {
+    const el = this._myFile.current;
+    if (el.files.length > 0) {
+      console.log("get file");
+      let formData = new FormData();
+      formData.append("photo", el.files[0]);
+    } else {
+      console.log("not file");
+    }
   };
 }
 const PhotoHeaderConnect = connect(
